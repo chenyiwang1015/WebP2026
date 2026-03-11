2af327048970afa1424f1d4ce5a33a31ff62bb62
chenyiwang1015 <chenyiwang1015@gmail.com>
2026-03-11 15:36:29 +0800

    Add files via upload

---
+++ ex1/urls.py
@@ -0,0 +1,6 @@
+﻿from django.urls import path
+from .views import MyHelloView
+
+urlpatterns = [
+    path('myhello/', MyHelloView.as_view()),
+]
---
+++ ex1/views.py
@@ -0,0 +1,11 @@
+﻿from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class MyHelloView(APIView):
+    def get(self, request):
+        name = request.query_params.get('name')
+        if name is None or name == "":
+            return Response({"res": "parameter: name is None"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response({"res": f"Hello {name}"}, status=status.HTTP_200_OK)
